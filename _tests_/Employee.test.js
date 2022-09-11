a4486ad2557e81d3adb13a30d4eb8621379dbeac
dre7283 <Dre7283@gmail.com>
2022-09-11 16:57:22 -0400

    adding Employee test

---
+++ _tests_/Employee.test.js
@@ -0,0 +1,31 @@
+const Employee = require("../lib/Employee");
+
+test("Can instantiate Employee instance", () => {
+    const e = new Employee();
+    expect(typeof (e)).toBe("object");
+});
+
+test("Can set name via constructor arguments", () => {
+    const name = "Kiley";
+    const e = new Employee(name);
+    expect(e.name).toBe(name)
+});
+
+test("Can set id via constructor arguments", () => {
+    const testValue = 100;
+    const e = new Employee("Bar", testValue);
+    expect(e.id).toBe(testValue);
+});
+
+test("Can set email via constructor argument", () => {
+    const testValue = "test@test.com";
+    const e = new Employee("Bar", 1, testValue);
+    expect(e.email).toBe(testValue);
+});
+
+test ("Can get name via getName()", () => {
+    const testValue = "Kiley";
+    const e = new Employee(testValue);
+    expect(e.getName()).toBe(testValue);
+});
+
