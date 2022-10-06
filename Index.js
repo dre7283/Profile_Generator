1c58483563ac36f345332bc611ba95ab027f9f9c
dre7283 <Dre7283@gmail.com>
2022-10-06 18:59:49 -0400

    redo templates

--- Index.js
+++ Index.js
@@ -5,8 +5,8 @@ const inquirer = require('inquirer');
 const path = require('path');
 const fs = require('fs');
 
-const DIST_DIR = path.resolve(__dirname, 'dist');
-const distPath = path.join(DIST_DIR, 'team.html');
+const OUTPUT_DIR = path.resolve(__dirname, 'output');
+const outputPath = path.join(OUTPUT_DIR, 'team.html');
 
 const render = require('./src/template.js');
 
